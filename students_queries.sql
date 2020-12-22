bc8db54aa916ab47aee0c11a11ac8784402ab8bb
plashy <72363241+plashy@users.noreply.github.com>
2020-12-22 11:42:17 -0500

    Add files via upload

---
+++ students_queries.sql
@@ -0,0 +1,19 @@
+/* highest_student_gpa */
+"SELECT MAX(students.gpa) FROM students;"
+
+
+/* lowest_student_gpa */
+"SELECT MIN(students.gpa) FROM students;"
+
+
+/* average_student_gpa */
+"SELECT AVG(students.gpa) FROM students;"
+
+
+/*  total_tardies_for_all_students */
+SELECT SUM(students.tardies) FROM students;
+
+
+/* average_gpa_for_9th_grade */
+SELECT AVG(students.gpa) FROM students WHERE students.grade==9;
+
